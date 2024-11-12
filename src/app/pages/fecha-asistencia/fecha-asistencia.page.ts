153ebde2772075aa25a078f017a9420c92282cc7
Schwarzesmak <mat.gonzalez6@duocuc.cl>
2024-11-12 00:12:24 -0300

    listoke

--- src/app/pages/fecha-asistencia/fecha-asistencia.page.ts
+++ src/app/pages/fecha-asistencia/fecha-asistencia.page.ts
@@ -1,15 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-fecha-asistencia',
   templateUrl: './fecha-asistencia.page.html',
   styleUrls: ['./fecha-asistencia.page.scss'],
 })
-export class FechaAsistenciaPage implements OnInit {
+export class FechaAsistenciaPage {
+  selectedDate: string = '';
+  selectedSection: string = '';
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
-  ngOnInit() {
+  onDateSelected(event: any) {
+    this.selectedDate = event.detail.value; // Obtener la fecha seleccionada
   }
 
+  onSectionSelected(event: any) {
+    this.selectedSection = event.detail.value; // Obtener la sección seleccionada
+    console.log('Sección seleccionada:', this.selectedSection);
+  }
+
+  confirmarAsistencia() {
+    if (this.selectedDate && this.selectedSection) {
+      console.log('Asistencia marcada para la fecha:', this.selectedDate);
+      console.log('Sección seleccionada:', this.selectedSection);
+      // Navegar a la página del código QR
+      this.navCtrl.navigateForward('/qr-code');
+    } else {
+      console.log('Por favor selecciona una fecha y una sección.');
+    }
+  }
 }
