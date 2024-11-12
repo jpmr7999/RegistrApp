c0a9a77ebbc6267f2e43751638cf45310b3478e9
Schwarzesmak <mat.gonzalez6@duocuc.cl>
2024-11-12 13:47:25 -0300

    codigo firebase lito

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+    production: true,
+    firebaseConfig : {
+    apiKey: "AIzaSyBPxhsWqYK6OMUZcttMnQ1-LAH_ujQvRck",
+    authDomain: "registradoritap.firebaseapp.com",
+    projectId: "registradoritap",
+    storageBucket: "registradoritap.firebasestorage.app",
+    messagingSenderId: "976226209676",
+    appId: "1:976226209676:web:84bc6541e7eae7957c90a9",
+    measurementId: "G-4WP4PVM8WC"
+  }
 };
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,8 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: true,
+  firebaseConfig : {
+  apiKey: "AIzaSyBPxhsWqYK6OMUZcttMnQ1-LAH_ujQvRck",
+  authDomain: "registradoritap.firebaseapp.com",
+  projectId: "registradoritap",
+  storageBucket: "registradoritap.firebasestorage.app",
+  messagingSenderId: "976226209676",
+  appId: "1:976226209676:web:84bc6541e7eae7957c90a9",
+  measurementId: "G-4WP4PVM8WC"
+}
 };
+
 
 /*
  * For easier debugging in development mode, you can import the following file
