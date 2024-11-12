1a50b1b2380cce6771200388f40f855179401040
Schwarzesmak <mat.gonzalez6@duocuc.cl>
2024-11-12 12:25:16 -0300

    principal listoo

--- src/app/pages/principal/principal.page.ts
+++ src/app/pages/principal/principal.page.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { NavController } from '@ionic/angular';
 
 @Component({
   selector: 'app-principal',
   templateUrl: './principal.page.html',
   styleUrls: ['./principal.page.scss'],
 })
-export class PrincipalPage implements OnInit {
+export class PrincipalPage {
 
-  constructor() { }
+  constructor(private navCtrl: NavController) {}
 
-  ngOnInit() {
+  // Va a la página de selección de fecha de asistencia
+  goToFechaAsistenciaPage() {
+    this.navCtrl.navigateForward('/fecha-asistencia');
   }
 
+  // Función para cerrar sesión
+  logout() {
+    this.navCtrl.navigateRoot('/home'); // Redirige a la página de inicio
+  }
 }
