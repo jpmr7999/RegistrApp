b6060b64063eafe5e624efbc059b521ca5df62d0
Schwarzesmak <mat.gonzalez6@duocuc.cl>
2024-11-30 13:44:28 -0300

    aun no quiero cagarla

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,10 +1,10 @@
 export const environment = {
-    production: true,
-    firebaseConfig : {
+  production: false,
+  firebaseConfig: {
     apiKey: "AIzaSyBPxhsWqYK6OMUZcttMnQ1-LAH_ujQvRck",
     authDomain: "registradoritap.firebaseapp.com",
     projectId: "registradoritap",
-    storageBucket: "registradoritap.firebasestorage.app",
+    storageBucket: "registradoritap.appspot.com",
     messagingSenderId: "976226209676",
     appId: "1:976226209676:web:84bc6541e7eae7957c90a9",
     measurementId: "G-4WP4PVM8WC"
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,17 +3,18 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
-  firebaseConfig : {
-  apiKey: "AIzaSyBPxhsWqYK6OMUZcttMnQ1-LAH_ujQvRck",
-  authDomain: "registradoritap.firebaseapp.com",
-  projectId: "registradoritap",
-  storageBucket: "registradoritap.firebasestorage.app",
-  messagingSenderId: "976226209676",
-  appId: "1:976226209676:web:84bc6541e7eae7957c90a9",
-  measurementId: "G-4WP4PVM8WC"
-}
+  production: false,
+  firebaseConfig: {
+    apiKey: "AIzaSyBPxhsWqYK6OMUZcttMnQ1-LAH_ujQvRck",
+    authDomain: "registradoritap.firebaseapp.com",
+    projectId: "registradoritap",
+    storageBucket: "registradoritap.appspot.com",
+    messagingSenderId: "976226209676",
+    appId: "1:976226209676:web:84bc6541e7eae7957c90a9",
+    measurementId: "G-4WP4PVM8WC"
+  }
 };
+
 
 
 /*
